d9da11d334d6ea8d261842bbaa2877cb8523483a
Suresh-Androd <surya7developer@gmail.com>
2024-09-14 23:59:13 +0530

    - For loops & variation - Step variation - Until variation - downTo variation

---
+++ app/src/main/java/com/android/topic/logicalprograms/kotlinSeries/Loops.kt
@@ -0,0 +1,41 @@
+package com.android.topic.logicalprograms.kotlinSeries
+
+// Loops are use to execute same statement again and again until condition get false
+
+fun main() {
+
+    println("Normal for loop")
+    //(1) Normal for loop with increment 1 value
+    for (i in 1..10) {
+        println(i)
+    }
+
+    println("Step variation")
+    //(2) We can plus more step instead of 1 increment every time. like below output
+    // 1, 3, 5, 7, 9
+    for (i in 1..10 step 2) {
+        println(i)
+    }
+
+    println("Until variation")
+    //(3) Now we check for loop with until variation
+    // Basically it will not include upper bound into range
+    // Ex: if we write for(i in 1 until 5) output : 1, 2, 3, 4
+    for (i in 1 until 5) {
+        println(i)
+    }
+
+    println("Step with until variation")
+    //(4) We can also use step variation with until.
+    for (i in 1 until 10 step 2) {
+        println(i)
+    }
+
+    println("downTo variation")
+    //(5) We have downTo variation which is used to execute statement top to bottom
+    // Ex : 10 to 2 - for(i in 10 downTo 1) output : 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+    // We can also use step variation with downTo
+    for (i in 10 downTo 1) {
+        println(i)
+    }
+}
